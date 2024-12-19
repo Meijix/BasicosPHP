152d068b6f423734e44e8496bb01b60587c58a11
Meijix <nmejia30@ciencias.unam.mx>
2024-12-19 17:06:42 -0600

    Improve style and HTMLs

--- TareaFormulario_NEMB/logout.php
+++ TareaFormulario_NEMB/logout.php
@@ -1,3 +1,22 @@
 <script>
     alert("¡Se ha cerrado sesion!");
 </script>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
+    <title>Sesion cerrada</title>
+</head>
+<body>
+    <h1>¡Hasta luego!</h1>
+    <p>¡Gracias por visitarnos!</p>
+    <div>
+        <img src="https://media.giphy.com/media/3o7TKz9bX9v6hZ8lIc/giphy.gif" alt="Gif de despedida">
+    </div>
+
+    <a href="login.php">Iniciar sesion</a>
+    <a href="formulario.php">Registrarse</a>
+</body>
+</html>
