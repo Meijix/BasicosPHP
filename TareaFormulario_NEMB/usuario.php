920222f2845e0beab012f525bd4df27087ed886d
Meijix <nmejia30@ciencias.unam.mx>
2024-12-20 01:45:19 -0600

    agregar otros usuarios precargados??

--- TareaFormulario_NEMB/usuario.php
+++ TareaFormulario_NEMB/usuario.php
@@ -10,6 +10,17 @@ $_SESSION['alumnosRegistrados'] = [
         "fecha_nac" => "2020-01-01"
     ]
 ];
+/* [
+    2 => [
+        'num_cta' => 317737822,
+        'nombre' => "Natalia",
+        "primer_apellido" => "Edith",
+        "segundo_apellido" => "Mejia",
+        "genero" => "F",
+        "contrasena" => "naty123",
+        "fecha_nac" => "2001-01-01"
+    ]
+]; */
 
 /* print_r($_SESSION); */
 
